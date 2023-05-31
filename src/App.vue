a86681c3fa5b05b372738a5a2b9f7472617d926f
AdnanCausevic <adnan.ado.causevic1921@gmail.com>
2023-05-31 21:45:50 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+    <div id="app">
+        <LayoutPage></LayoutPage>
+    </div>
+</template>
+<script>
+  import LayoutPage from './layout/LayoutPage.vue'
+  export default {
+    components: {
+      LayoutPage
+    }
+  }
+</script>
+
+
+
